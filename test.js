e6d9766b48c82ae16a34aa4ea985b4d84af96ad7
Jonathan Kemp <kempdogg@gmail.com>
2016-03-22 21:49:08 -0400

    Bump style-selector module.

--- test.js
+++ test.js
@@ -3,12 +3,12 @@
 'use strict';
 
 var assert = require('assert'),
-    Selector = require('style-selector'),
+    selector = require('style-selector'),
     Property = require('./');
 
 describe('Property', function () {
     it('should return an object', function () {
-        var bodySelector = new Selector('body'),
+        var bodySelector = selector('body'),
             prop = new Property('font-family', 'Arial', bodySelector);
         assert(prop);
         assert.equal(prop.prop, 'font-family');
@@ -20,7 +20,7 @@ describe('Property', function () {
 
 describe('Property.toString', function () {
     it('should return a css declaration', function () {
-        var bodySelector = new Selector('body'),
+        var bodySelector = selector('body'),
             prop = new Property('font-family', 'Arial', bodySelector);
         assert.equal(prop.toString(), 'font-family: Arial;');
     });
@@ -28,8 +28,8 @@ describe('Property.toString', function () {
 
 describe('Property.compare', function () {
     it('should return the more specific of two properties', function () {
-        var bodySelector = new Selector('body'),
-            h1Selector = new Selector('h1'),
+        var bodySelector = selector('body'),
+            h1Selector = selector('h1'),
             propA = new Property('font-family', 'Arial', bodySelector),
             propB = new Property('color', 'blue', h1Selector),
             winner = propA.compare(propB);
